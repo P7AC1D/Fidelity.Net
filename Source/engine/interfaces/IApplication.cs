0d3b2d8260ec4a5c3a133114fc8f3fef0f43d2a7
Warren Roberson <warren.roberson@gmail.com>
2024-05-01 19:37:50 +0200

    Renamed src folder.

---
+++ Source/engine/interfaces/IApplication.cs
@@ -0,0 +1,13 @@
+using Silk.NET.Input;
+using Silk.NET.Maths;
+
+namespace Fidelity;
+
+public interface IApplication
+{
+  public void Run();
+  public void Load();
+  public void Render(double dt);
+  public void Update(double dt);
+  public void KeyDown(IKeyboard arg1, Key arg2, int arg3);
+}
